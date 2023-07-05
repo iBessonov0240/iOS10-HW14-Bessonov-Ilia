fa2172926bb86db3620738c954f08631dabab559
Ilia <ilya2491@gmail.com>
2023-07-05 12:59:23 +0300

    Initial Commit

---
+++ iOS10-HW14-Bessonov Ilia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS10-HW14-Bessonov Ilia
+//
+//  Created by i0240 on 05.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
